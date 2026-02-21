ae28480d372b26ab86d9f3074c60796f85d99aad
euanleith <leithe@tcd.ie>
2026-02-21 17:11:07 +0000

    godot update

--- src/Actor/Controllers/EnemyCombatController.cs
+++ src/Actor/Controllers/EnemyCombatController.cs
@@ -1,10 +1,7 @@
-using MonsterCounty.Actor.Decisions;
-using MonsterCounty.State;
-
 namespace MonsterCounty.Actor.Controllers
 {
-    public partial class EnemyCombatController : CombatController
-    {
-        protected override Decision<CombatState> LoadDecision() => new RandomDecision<CombatState>();
-    }
+	public partial class EnemyCombatController : CombatController
+	{
+		
+	}
 }
