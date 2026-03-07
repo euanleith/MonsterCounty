671ab518c69549a188f52da6c7996f8947d3c724
euanleith <leithe@tcd.ie>
2026-03-07 11:02:38 +0000

    make decision resources

--- src/Actor/Decisions/Combat/RandomCombatDecision.cs
+++ src/Actor/Decisions/Combat/RandomCombatDecision.cs
@@ -7,12 +7,17 @@ using static MonsterCounty.Utilities.EnumUtilities;
 
 namespace MonsterCounty.Actor.Decisions.Combat
 {
+    [GlobalClass]
     public partial class RandomCombatDecision : RandomDecision<ActionController<CombatActor>, CombatActor>
     {
+        [Export] private bool _changeCombatPosition;
+        
         public override CombatChoice Choose(ActionController<CombatActor> controller)
-        {
+        { 
             var randomAction = base.Choose(controller).Action;
-            var position = GetRandomEnumValue<CombatPosition>();
+            var position = _changeCombatPosition ? 
+                GetRandomEnumValue<CombatPosition>() : 
+                (controller as CombatController).CombatPosition;
             return new CombatChoice(randomAction as CombatAction, position);
         }
     }
